e5cf5d3aab9d22aa5c7105ec7ccbef59035dc314
gsd1601 <guido@vitalite.de>
2021-01-16 17:25:37 +0100

    Allererser Commit

---
+++ ProcessingGravitation02/src/de/gsc/processing/Gravitation.java
@@ -0,0 +1,9 @@
+package de.gsc.processing;
+
+public class Gravitation {
+
+	public static void main(String[] args) {
+		System.out.println("Starte Gravitation! ");
+	}
+
+}
---
+++ ProcessingGravitation02/src/de/gsc/processing/package-info.java
@@ -0,0 +1,1 @@
+package de.gsc.processing;
